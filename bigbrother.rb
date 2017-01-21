dea1ab5423b40b656ee9cae5c5b238f4767593b9
florent angles <florent.angles@hei.fr>
2017-01-21 19:02:19 +0100

    change format

--- bigbrother.rb
+++ bigbrother.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 
+set :template_engines, {
+  :css=>[:scss],
+  :xml=>[],
+  :js=>[],
+  :html=>[:haml],
+  :all=>[:haml],
+  :json=>[]
+}
+
 get '/' do
   haml :index, layout: :layout
 end
